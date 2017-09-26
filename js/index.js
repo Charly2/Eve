30268cfce937b166792f98caea7dd4070e78175c
Charly_Network <papapitufo10@gmail.com>
2017-09-26 01:47:53 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var love = setInterval(function(){
+  var r_num = Math.floor(Math.random() * 40) + 1;
+	var r_size = Math.floor(Math.random() * 65) + 10;
+	var r_left = Math.floor(Math.random() * 100) + 1;
+	var r_bg = Math.floor(Math.random() * 25) + 100;
+  var r_time = Math.floor(Math.random() * 5) + 5;
+  
+  $('.bg_heart').append("<div class='heart' style='width:"+r_size+"px;height:"+r_size+"px;left:"+r_left+"%;background:rgba(255,"+(r_bg-25)+","+r_bg+",1);-webkit-animation:love "+r_time+"s ease;-moz-animation:love "+r_time+"s ease;-ms-animation:love "+r_time+"s ease;animation:love "+r_time+"s ease'></div>");
+  
+  $('.bg_heart').append("<div class='heart' style='width:"+(r_size-10)+"px;height:"+(r_size-10)+"px;left:"+(r_left+r_num)+"%;background:rgba(255,"+(r_bg-25)+","+(r_bg+25)+",1);-webkit-animation:love "+(r_time+5)+"s ease;-moz-animation:love "+(r_time+5)+"s ease;-ms-animation:love "+(r_time+5)+"s ease;animation:love "+(r_time+5)+"s ease'></div>");
+  
+  $('.heart').each(function(){
+    var top = $(this).css("top").replace(/[^-\d\.]/g, '');
+    var width = $(this).css("width").replace(/[^-\d\.]/g, '');
+    if(top <= -100 || width >= 150){
+      $(this).detach();
+    }
+  });
+},500);
+
+
+let i = 1, cad = 'url(img%i.jpg)';
+
+var img = setInterval(function (argument) {
+  if(i==6){
+    i=1;
+  }  
+  var a = cad.replace("%i",i)
+ /* var img = document.getElementById('imgs');
+
+
+    console.log(a);
+ img.style.background=a
+*/
+
+    $('#imgs').css({
+      'background': a,
+      'background-size': 'cover'
+    });
+
+
+  i++;
+
+},3000);
